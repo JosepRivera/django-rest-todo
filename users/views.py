ec1c2bc7154484a41f7a38a0239084d379ee1665
Josep Rivera <josepdanton1518@gmail.com>
2025-09-30 12:49:08 -0500

    Add user registration functionality with serializer and view; update URL routing

--- users/views.py
+++ users/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import AllowAny
+from django.contrib.auth.models import User
+from rest_framework.serializers import ModelSerializer
 
-# Create your views here.
+
+# Serializer para crear usuarios
+class RegisterSerializer(ModelSerializer):
+    class Meta:
+        model = User
+        fields = ("username", "email", "password")
+        extra_kwargs = {"password": {"write_only": True}}
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username=validated_data["username"],
+            email=validated_data["email"],
+            password=validated_data["password"],
+        )
+        return user
+
+
+# Vista para el registro
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = RegisterSerializer
+    permission_classes = [AllowAny]  # cualquiera puede registrarse
